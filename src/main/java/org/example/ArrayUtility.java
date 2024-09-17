660aa9d968a15050ff7e336f951f9b5491e50575
qrwas <aleksandr.vernigora93@gmail.com>
2024-09-17 21:00:42 +0300

    Merge: LR_2
    commit 974e4bdb320b66fd90f711ea64a622bd2ff18092
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:58:54 2024 +0300
    
        Add FindGeometricMean
    
    commit b2d4d2687855ab4c3ee2cf5a76add4505b0b0f97
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:48:06 2024 +0300
    
        Add arifmetic mean
    
    commit a1a58c89e3ca10a758aa96d7103710fef967fa21
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:42:56 2024 +0300
    
        Add FindMaxValue
    
    commit bf6cb364ee6f0c8f19a2519c87bee1154e284327
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:40:06 2024 +0300
    
        add FindMinValue
    
    commit 38018547fa8e4aae31d197376e8ccea1e2e4abe2
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:36:04 2024 +0300
    
        create ArrayUtility
    
    commit b19cc3c90be7d6ff94f25f9fb4a7dd539e79e6a9
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:35:33 2024 +0300
    
        Rename class to CreateArray
    
    commit a635b03b0094b15cab94c6367f917692608a44fe
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:34:02 2024 +0300
    
        fix
    
    commit 408b286bbea4a4d43fba60a1858b2c217d8bb862
    Author: qrwas <aleksandr.vernigora93@gmail.com>
    Date:   Tue Sep 17 20:33:37 2024 +0300
    
        Add base LabTwo class for create array

---
+++ src/main/java/org/example/ArrayUtility.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class ArrayUtility {
+    public void FindMinValue(int[][] arr) {
+        int min = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] < min) {
+                    min = arr[i][j];
+                }
+            }
+        }
+        System.out.print("мінімальне значення: " + min + "\n");
+    }
+
+    public void FindMaxValue(int[][] arr) {
+        int max = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.print("максимальне значення: " + max + "\n");
+    }
+
+    public void FindArithmeticMean(int[][] arr) {
+        int countElement = 0;
+        int sumAllElements = 0;
+        for (int i = 0; i < arr.length; i++) {
+            countElement += arr[i].length;
+            for (int j = 0; j < arr[i].length; j++) {
+                sumAllElements += arr[i][j];
+            }
+        }
+        float result = (float) sumAllElements / countElement;
+        System.out.print("Середнє арифметичне значення: " + result + "\n");
+    }
+
+    public void FindGeometricMean(int[][] arr) {
+        int countElement = 0;
+        double multAllElements = 1;
+        for (int i = 0; i < arr.length; i++) {
+            countElement += arr[i].length;
+            for (int j = 0; j < arr[i].length; j++) {
+                multAllElements *= arr[i][j];
+            }
+        }
+
+        float result = (float) Math.pow(multAllElements, (1/(float)countElement));
+        System.out.print("Середнє геометричне значення: " + result + "\n");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
+        int array[][];
+        CreateArray createArray = new CreateArray();
+        array = createArray.CreateArray();
 
+        ArrayUtility arrayUtility = new ArrayUtility();
+
+        arrayUtility.FindMinValue(array);
+        arrayUtility.FindMaxValue(array);
+        arrayUtility.FindArithmeticMean(array);
+        arrayUtility.FindGeometricMean(array);
     }
 }
